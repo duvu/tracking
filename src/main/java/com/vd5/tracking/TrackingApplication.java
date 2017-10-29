6f86c0e90574a3d2f94c0edd766d2788f80093aa
beou <hoaivubk@gmail.com>
2017-10-29 16:46:55 +0700

    Separated dcs vs web

--- src/main/java/com/vd5/tracking/TrackingApplication.java
+++ src/main/java/com/vd5/tracking/TrackingApplication.java
@@ -1,6 +1,5 @@
 package com.vd5.tracking;
 
-import com.vd5.tracking.dcs.ServerManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,6 +9,5 @@ public class TrackingApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(TrackingApplication.class, args);
-		context.getBean(ServerManager.class).init().start();
 	}
 }
